542f57c21f86e8ee0edb4a46e0ac9a1c9cc3076b
masatoshi sugita <mr.f-r.ms621@i.softbank.jp>
2018-09-10 22:50:20 +0900

    make it better

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
 
+  default_scope -> { order(created_at: :desc) }
+
   def self.search(search)
     if search
       Post.where(['sick_name LIKE ?', "%#{search}%"])
